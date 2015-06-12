cfbb21370a5641d5768d487df1c1c82e4f3116aa
TheFakeMontyOnTheRun <danielmonteiro@id.uff.br>
2015-06-11 21:11:05 -0300

    Remove old spells from dummy wizards
    To make room to the new ones. Also, the rules of the game are about to change.

--- sup-wiz-core-java/src/main/java/br/odb/supremewizardry/core/DummyWizard1.java
+++ sup-wiz-core-java/src/main/java/br/odb/supremewizardry/core/DummyWizard1.java
@@ -1,17 +1,10 @@
 package br.odb.supremewizardry.core;
 
-import br.odb.supremewizardry.core.cards.legacy.DeceptionSpell;
-import br.odb.supremewizardry.core.cards.legacy.MagicDaggerSpell;
-import br.odb.supremewizardry.core.cards.legacy.SiberianIceWindSpell;
 
 public class DummyWizard1 extends Wizard {
 
 	public DummyWizard1() {
 		super( "Rasputin", "Powerful and mysterious" );
-		
-		spells.add( new DeceptionSpell() );
-		spells.add( new MagicDaggerSpell() );
-		spells.add( new SiberianIceWindSpell() );
 	}
 	
 	
--- sup-wiz-core-java/src/main/java/br/odb/supremewizardry/core/DummyWizard2.java
+++ sup-wiz-core-java/src/main/java/br/odb/supremewizardry/core/DummyWizard2.java
@@ -1,17 +1,10 @@
 package br.odb.supremewizardry.core;
 
-import br.odb.supremewizardry.core.cards.legacy.AlchemySpell;
-import br.odb.supremewizardry.core.cards.legacy.FireballSpell;
-import br.odb.supremewizardry.core.cards.legacy.GuidanceSpell;
 
 public class DummyWizard2 extends Wizard {
 
 	public DummyWizard2() {
-		super( "Merlin", "Wise and resourceful" );
-		
-		spells.add( new GuidanceSpell() );
-		spells.add( new FireballSpell() );
-		spells.add( new AlchemySpell() );
+		super( "Merlin", "Wise and resourceful" );		
 	}
 	
 	@Override
