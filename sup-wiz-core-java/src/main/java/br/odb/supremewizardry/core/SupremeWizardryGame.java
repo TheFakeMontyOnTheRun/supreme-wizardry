a669cdb9ad0149576acd91863506a4f2a594e724
TheFakeMontyOnTheRun <danielmonteiro@id.uff.br>
2015-06-17 20:33:26 -0300

    Adding basic multiplayer client
    Still disabled, for now.

--- sup-wiz-core-java/src/main/java/br/odb/supremewizardry/core/SupremeWizardryGame.java
+++ sup-wiz-core-java/src/main/java/br/odb/supremewizardry/core/SupremeWizardryGame.java
@@ -3,6 +3,9 @@ package br.odb.supremewizardry.core;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 import br.odb.gameapp.ConsoleApplication;
 import br.odb.supremewizardry.core.card.*;
 import br.odb.supremewizardry.core.commands.EndTurnCommand;
@@ -10,7 +13,25 @@ import br.odb.supremewizardry.core.commands.PickCardCommand;
 import br.odb.supremewizardry.core.commands.UseCardCommand;
 import br.odb.supremewizardry.core.commands.DisplayStatusCommand;
 
-public class SupremeWizardryGame extends ConsoleApplication {
+public class SupremeWizardryGame extends ConsoleApplication implements MultiplayerClient {
+
+public static enum Teams {
+	EMPTY("[#]"), ICE("[O]"), FIRE("[X]");
+
+	String representation;
+
+	Teams(String representation) {
+		this.representation = representation;
+	}
+
+	@Override
+	public String toString() {
+		return representation;
+	}
+}
+
+
+
 
 	Card[] possibleCards = new Card[]{
 //		new ArcadeShieldCard(),
@@ -37,6 +58,14 @@ public class SupremeWizardryGame extends ConsoleApplication {
 	private Wizard wizard2;
 	public Wizard currentWizard;
 	final public List< Card > tableCards = new ArrayList<>(); 
+	private ServerProxy serverProxy = new ServerProxy(
+			"http://localhost:8080/multiplayer-server");
+	private int playerId;
+	private int gameId;
+	private Teams winnerTeam;
+	private Teams currentTeam;
+	private Teams localPlayerTeam;
+
 	
 	public SupremeWizardryGame() {
 		super();
@@ -54,7 +83,13 @@ public class SupremeWizardryGame extends ConsoleApplication {
 		
 		tableCards.add( 0, new LifeStealCard());
 		wizard1.takeCards( 1, tableCards);
+		//requestGameFromServer();
 	}
+
+	public void requestGameFromServer() {
+		serverProxy.startConnection(this);
+	}
+
 	
 	private void shuffleCardsIntoTable() {
 		for ( int c = 0; c < 50; ++c ) {
@@ -135,4 +170,91 @@ public class SupremeWizardryGame extends ConsoleApplication {
 	protected void doQuit() {
 		System.out.println( 0 );
 	}
+	
+	@Override
+	public void receiveGameId(int gameId) {
+		this.gameId = gameId;
+		getClient().printVerbose("got game id: " + gameId);
+	}
+
+	@Override
+	public void receivePlayerId(int playerId) {
+		this.playerId = playerId;
+		getClient().printVerbose("got player id: " + playerId);
+	}
+
+	@Override
+	public void receiveTeamId(int teamId) {
+		this.localPlayerTeam = Teams.values()[teamId];
+		getClient().printVerbose("got local player team: " + localPlayerTeam);
+	}
+
+	@Override
+	public void receiveMove(Node gameNode) {
+
+		Teams[] teams = Teams.values();
+
+		int i = 0;
+
+		NodeList nodeLst = gameNode.getChildNodes();
+
+		for (int s = 0; s < nodeLst.getLength(); s++) {
+
+			Node fstNode = nodeLst.item(s);
+
+			if (fstNode != null) {
+
+				if (fstNode.getNodeType() == Node.ELEMENT_NODE) {
+
+					if (fstNode.getNodeName().equals("state")) {
+
+						String statusData = fstNode.getTextContent();
+
+
+					} else if (fstNode.getNodeName().equals("current")) {
+						this.currentTeam = teams[Integer.parseInt(fstNode
+								.getTextContent())];
+					} else if (fstNode.getNodeName().equals("winner")) {
+						this.winnerTeam = teams[Integer.parseInt(fstNode.getTextContent())];
+					}
+				}
+			}
+		}
+
+		if (this.winnerTeam != teams[0]) {
+
+			getClient().printNormal("team " + winnerTeam + " wins the match");
+		}
+	}
+
+	@Override
+	public void endOfTurn() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void allowedToSendMove() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void receiveStatus() {
+		serverProxy.requestUpdate(this);
+	}
+
+	@Override
+	public int getAppId() {
+		return gameId;
+	}
+
+	public void sendRemoteMove(int x, int y) {
+		serverProxy.sendData(x, y, this);
+
+	}
+
+	@Override
+	public int getUserId() {
+		return playerId;
+	}	
 }
